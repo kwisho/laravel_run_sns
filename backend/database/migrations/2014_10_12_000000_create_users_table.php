a51df24e1bb37e3016bd3b23f1b190a3792c4516
kwisho <kwi.shota0912@gmail.com>
2021-09-18 15:58:06 +0900

    <記事一覧画面を表示>

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,18 +11,19 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+
+     public function up()
+     {
+         Schema::create('users', function (Blueprint $table) {
+             $table->bigIncrements('id');
+             $table->string('name')->unique();
+             $table->string('email')->unique();
+             $table->timestamp('email_verified_at')->nullable();
+             $table->string('password')->nullable();
+             $table->rememberToken();
+             $table->timestamps();
+         });
+     }
 
     /**
      * Reverse the migrations.
